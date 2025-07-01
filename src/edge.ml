5e9949ecf027e5280636a26e0c16681190de6920
May Takahashi <maishatakahashi@gmail.com>
2025-07-01 20:58:12 +0000

    refactor edge detection

--- src/edge.ml
+++ src/edge.ml
@@ -6,69 +6,27 @@ let transform image =
   let blur = Blur.transform gray ~radius:2 in
   let max = Image.max_val gray in
   Image.mapi blur ~f:(fun ~x ~y _pixel ->
-    let get scale ~x_adj ~y_adj : int =
-      scale * Pixel.red (Image.get blur ~x:x_adj ~y:y_adj)
+    let index_diff =
+      [ -1, -1; 0, -1; 1, -1; -1, 0; 0, 0; 1, 0; -1, 1; 0, 1; 1, 1 ]
     in
-    let x_in_range num = 0 < x + num && x + num < Image.width blur in
-    let y_in_range num = 0 < y + num && y + num < Image.height blur in
+    let pixel_values =
+      List.map index_diff ~f:(fun (x_adj, y_adj) ->
+      let x_in_range = 0 < x + x_adj && x + x_adj < Image.width blur in
+      let y_in_range = 0 < y + y_adj && y + y_adj < Image.height blur in
+      if x_in_range && y_in_range then 
+        Pixel.red (Image.get blur ~x:(x + x_adj) ~y:(y + y_adj)) else 0)
+    in
+    let scale n pixel_index = n * List.nth_exn pixel_values (pixel_index-1) in
     let g_x =
-      Float.of_int
-        ((if y_in_range (-1) && x_in_range (-1)
-          then get (-1) ~x_adj:(x - 1) ~y_adj:(y - 1)
-          else 0)
-         + (if x_in_range (-1) then get (-2) ~x_adj:(x - 1) ~y_adj:y else 0)
-         + (if y_in_range 1 && x_in_range (-1)
-            then get (-1) ~x_adj:(x - 1) ~y_adj:(y + 1)
-            else 0)
-         + (if y_in_range (-1) && x_in_range 1
-            then get 1 ~x_adj:(x + 1) ~y_adj:(y - 1)
-            else 0)
-         + (if x_in_range 1 then get 2 ~x_adj:(x + 1) ~y_adj:y else 0)
-         +
-         if y_in_range 1 && x_in_range 1
-         then get 1 ~x_adj:(x + 1) ~y_adj:(y + 1)
-         else 0)
+      Float.of_int (scale (-1) 1 + scale (-2) 4 + scale (-1) 7 + scale 1 3 + scale 2 6 + scale 1 9)
     in
     let g_y =
-      Float.of_int
-        ((if x_in_range (-1) && y_in_range (-1)
-          then get (-1) ~x_adj:(x - 1) ~y_adj:(y - 1)
-          else 0)
-         + (if y_in_range (-1) then get (-2) ~x_adj:x ~y_adj:(y - 1) else 0)
-         + (if x_in_range 1 && y_in_range (-1)
-            then get (-1) ~x_adj:(x + 1) ~y_adj:(y - 1)
-            else 0)
-         + (if x_in_range (-1) && y_in_range 1
-            then get 1 ~x_adj:(x - 1) ~y_adj:(y + 1)
-            else 0)
-         + (if y_in_range 1 then get 2 ~x_adj:x ~y_adj:(y + 1) else 0)
-         +
-         if x_in_range 1 && y_in_range 1
-         then get 1 ~x_adj:(x + 1) ~y_adj:(y + 1)
-         else 0)
+      Float.of_int(scale (-1) 1 + scale (-2) 2 + scale (-1) 3 + scale 1 7 + scale 2 8 + scale 1 9)
     in
     let gradient_mag = Float.sqrt (Float.square g_x +. Float.square g_y) in
     if Float.( >. ) gradient_mag (Float.of_int max *. 0.4)
     then max, max, max
     else 0, 0, 0)
-;;
-
-let%expect_test "transform" =
-  let output =
-    transform (Image.load_ppm ~filename:"../images/beach_portrait.ppm")
-  in
-  let expected =
-    Image.load_ppm ~filename:"../images/reference-beach_portrait_edge.ppm"
-  in
-  print_endline
-    (sprintf
-       "Output is off by %d of %d pixels"
-       (Image.compare output expected)
-       (Image.height expected * Image.width expected));
-  [%expect
-    {|
-    Output is off by 0 of 461600 pixels
-  |}]
 ;;
 
 let command =
